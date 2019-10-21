17156e2e02ef5ea78157976dd33c8873d57ec9f6
Thejas Gowda <tnanjundaiah@vmware.com>
2019-10-21 14:50:37 +0530

    Added form elements

--- multilayout/src/app/intelligence-instances/Add/add-edit-intelligence-instance/add-edit-intelligence-instance.component.ts
+++ multilayout/src/app/intelligence-instances/Add/add-edit-intelligence-instance/add-edit-intelligence-instance.component.ts
@@ -1,51 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-add-edit-intelligence-instance',
-  templateUrl: './add-edit-intelligence-instance.component.html',
-  styleUrls: ['./add-edit-intelligence-instance.component.scss']
+  selector: "app-add-edit-intelligence-instance",
+  templateUrl: "./add-edit-intelligence-instance.component.html",
+  styleUrls: ["./add-edit-intelligence-instance.component.scss"]
 })
 export class AddEditIntelligenceInstanceComponent implements OnInit {
   open = true;
+  savingInstance = false;
+  usageOptions = [
+    { id: "", name: "Select" },
+    { id: "1", name: "Demo" },
+    { id: "2", name: "Development" },
+    { id: "7", name: "Pilot / POC" },
+    { id: "4", name: "Production" },
+    { id: "5", name: "QA" },
+    { id: "6", name: "Trial" },
+    { id: "8", name: "Trial & Production" },
+    { id: "3", name: "UAT" }
+  ];
+
+  instanceStatus = [
+    { id: "1", name: "ACTIVE" },
+    { id: "2", name: "ARCHIVE" },
+    { id: "7", name: "DECOMMISSIONED" },
+    { id: "4", name: "IGNORED" }
+  ];
+
   model = new FormGroup({
-    basic: new FormControl(''),
-    container: new FormControl(''),
-    required: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern(/asdfasdf/)]),
+    basic: new FormControl(""),
+    container: new FormControl(""),
+    required: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(/asdfasdf/)
+    ])
   });
-  contactForm: FormGroup;
-  form: FormGroup = this.getReactiveForm();
-  constructor() {
-    this.contactForm = this.createFormGroup();
+
+  form: FormGroup;
+  constructor(private router: Router) {
+    this.form = this.getReactiveForm();
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 
   private getReactiveForm() {
     return new FormGroup({
-        first: new FormControl('Luke', Validators.required),
-        last: new FormControl('Skywalker', Validators.required),
-      contact: new FormGroup({
-        email: new FormControl(),
-        phone: new FormControl(),
-      }),
-      password: new FormGroup({
-        password: new FormControl(),
-        confirm: new FormControl(),
-      }),
+      fiendlyName: new FormControl("", Validators.required),
+      endpointUrl: new FormControl("", Validators.required),
+      nodeIdentifier: new FormControl("", Validators.required),
+      usageOptions: new FormControl("", Validators.required),
+      instanceStatus: new FormControl("1", Validators.required),
+      authorizationtUrl: new FormControl("", Validators.required),
+      clientId: new FormControl("", Validators.required),
+      clientSecret: new FormControl("", Validators.required),
     });
   }
 
-  createFormGroup() {
-    return new FormGroup({
-      personalData: new FormGroup({
-        email: new FormControl(),
-        mobile: new FormControl(),
-        country: new FormControl(),
-      }),
-      requestType: new FormControl(),
-      text: new FormControl(),
-    })
+  navigateToInstanceDashBoard(saving) {
+    this.savingInstance = saving;
+    let delay = saving ? 3000 : 0;
+    let self = this;
+    setTimeout(
+      () => self.router.navigateByUrl("/clr-intelligence-instances"),
+      delay
+    );
   }
 }
